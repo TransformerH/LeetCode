2eb5aa8b6d7f4a60224d61e753cec10a29797d63
Sheryl Han <SherylHan@example.com>
2018-05-18 14:49:15 +0800

    recursion, add a better solution

--- LeetCode16.cpp
+++ LeetCode16.cpp
@@ -25,3 +25,32 @@ int threeSumClosest(vector<int>& nums, int target) {
     sumOfThree(nums, target, sum, 0, res);
     return res;
 }
+
+// better one
+
+
+int threeSumClosest(vector<int>& nums, int target) {
+    if(nums.size() < 3)
+        return 0;
+    int res = nums[0] + nums[1] + nums[2];
+    sort(nums.begin(), nums.end());
+    for(int first = 0; first < nums.size() - 2; first++){
+        if(first > 0 && nums[first] == nums[first - 1])
+            continue;
+        int second = first+1;
+        int third = (int)nums.size() - 1;
+        while(second < third){
+            int curSum = nums[first] + nums[second] + nums[third];
+            if(curSum == target)
+                return curSum;
+            if(abs(target - res) > abs(target - curSum))
+                res = curSum;
+            if(target < curSum)
+                third--;
+            else
+                second++;
+        }
+        
+    }
+    return res;
+}
