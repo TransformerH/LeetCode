67b8bf03d320f7f8b7ede35b9ee274408487eb88
Sheryl Han <SherylHan@example.com>
2018-05-29 11:55:48 +0800

    review, recursion

---
+++ LeetCode236.cpp
@@ -0,0 +1,13 @@
+// recursion 
+// need to be reviewed
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(!root || !p || !q)
+        return NULL;
+    if(root == p || root == q)
+        return root;
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+    if(left && right)
+        return root;
+    return left ? left : right;
+}
